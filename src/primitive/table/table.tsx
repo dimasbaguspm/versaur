0b8102bc688580aef3f5e94c61c4a98096ef32cd
dimasbaguspm <dimas.bagus.pm1@gmail.com>
2025-08-05 06:08:47 +0700

    refactor(table): Table component to use HeaderItem and RowItem

--- src/primitive/table/table.tsx
+++ src/primitive/table/table.tsx
@@ -1,44 +1,43 @@
-import { useMemo, forwardRef } from 'react'
-import type { TableContextValue, TableProps } from './types'
+import { forwardRef } from 'react'
+import type { TableProps } from './types'
 import { cn } from '@/utils/cn'
 import { TableProvider } from './context'
 import {
   TableBody,
-  TableColumn,
   TableFooter,
   TableHeader,
+  TableHeaderItem,
   TableRow,
+  TableRowItem,
 } from './table.atoms'
 
 const TableRoot = forwardRef<HTMLDivElement, TableProps>(
-  ({ children, className, maxColumns = 12, ...props }, ref) => {
-    const value = useMemo(
-      () => ({ maxColumns }),
-      [maxColumns]
-    ) satisfies TableContextValue
-
-    return (
-      <TableProvider value={value}>
-        <div
-          ref={ref}
-          role='table'
-          className={cn(
-            'w-full overflow-x-auto rounded-lg border border-border',
-            className
-          )}
-          {...props}
-        >
-          {children}
-        </div>
-      </TableProvider>
-    )
-  }
+  ({ children, className, columns = 12, ...props }, ref) => (
+    <TableProvider value={{ columns }}>
+      <div
+        ref={ref}
+        role='table'
+        className={cn(
+          // Versaur design system: border, background, shadow, rounded, spacing
+          'w-full overflow-x-auto border border-border bg-background rounded-lg ',
+          'text-foreground',
+          'sm:rounded-xl',
+          'transition-colors',
+          className
+        )}
+        {...props}
+      >
+        {children}
+      </div>
+    </TableProvider>
+  )
 )
 
 export const Table = Object.assign(TableRoot, {
   Header: TableHeader,
+  HeaderItem: TableHeaderItem,
   Body: TableBody,
   Footer: TableFooter,
   Row: TableRow,
-  Column: TableColumn,
+  RowItem: TableRowItem,
 })
