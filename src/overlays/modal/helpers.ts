f183d2de2741d845640016db2bc2dbfa4c9d35d1
dimasbaguspm <dimas.bagus.pm1@gmail.com>
2025-08-18 06:24:03 +0700

    fix: adjust modal content styles for improved layout and responsiveness

--- src/overlays/modal/helpers.ts
+++ src/overlays/modal/helpers.ts
@@ -17,19 +17,18 @@ export const modalOverlayVariants = cva(
 
 export const modalContentVariants = cva(
   [
-    'z-61 bg-white rounded-lg shadow-xl fixed',
+    'absolute z-61 bg-white rounded-lg shadow-xl',
     'flex flex-col',
     'outline-none',
-    'mx-4',
-    'transition-all duration-200 ease-in-out ',
+    'transition-all duration-200 ease-in-out',
   ],
   {
     variants: {
       size: {
-        sm: 'w-[20rem] max-w-sm', // 20rem, 320px
-        md: 'w-[28rem] max-w-md', // 28rem, 448px
-        lg: 'w-[36rem] max-w-lg', // 36rem, 576px
-        'fit-content': 'w-fit max-w-full', // Fit content, no max width
+        sm: 'w-[20rem] max-w-[90%]', // 20rem, 320px
+        md: 'w-[28rem] max-w-[90%]', // 28rem, 448px
+        lg: 'w-[36rem] max-w-[90%]', // 36rem, 576px
+        'fit-content': 'w-fit max-w-[90%]', // Fit content, no max width
       },
       placement: {
         top: 'top-8 left-1/2 -translate-x-1/2',
