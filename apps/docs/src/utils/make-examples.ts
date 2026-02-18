3042d914da5a77b779f1d38747a9613c067d024a
dimasbaguspm <dimas.bagus.pm1@gmail.com>
2026-02-18 11:54:35 +0700

    refactor!:delete custom app docs

--- apps/docs/src/utils/make-examples.ts
+++
@@ -1,7 +0,0 @@
-export function makeExamples(section: { code: string; language: string }) {
-  return {
-    react: { code: section.code, language: section.language },
-    vue: { code: "", language: "vue" },
-    angular: { code: "", language: "angular" },
-  };
-}
